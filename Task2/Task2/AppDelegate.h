eeed906d31c400c6a4e1ec004d4f8fcb760453a7
Roman Kananovich <roman.kananovich@gmail.com>
2019-06-09 17:56:40 +0300

    initial

---
+++ Task2/Task2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task2
+//
+//  Created by Roma on 6/9/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task2/Task2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Task2
+//
+//  Created by Roma on 6/9/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
